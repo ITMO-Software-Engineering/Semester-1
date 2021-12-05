8024edb2b1a5027b44b2a3c5cc7ea860f4276eb1
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-12-05 16:52:18 +0300

    update

--- Information/Inf_lab_4/Lab4.py
+++ Information/Inf_lab_4/Lab4.py
@@ -1,29 +1,84 @@
 f_in = open('source file.yml', 'r', encoding='utf_8')
 f_out = open('output file.json', 'w', encoding='utf_8')
 
+bigBrackets = [0]*100
+middleBrackets = [0]*100
 lines = list()
 keyword = list()
-lineCounter = 0
+lineCounter = 1
+
 newLine = f_in.readline()
 while newLine:
     lines.append(newLine)
     lineCounter += 1
     newLine = f_in.readline()
-f_in.close()
+lines.append('')
+lines.append('')
+lines.append('')
+lineCounter += 3
 
-lastLevel = len(lines[0]) - len(lines[0].lstrip())
+print("lineCounter=", lineCounter)
 f_out.write("{\n")
+level = 0
+
+
+def write_space(level_num):
+    temp = level_num
+    while temp >= 0:
+        f_out.write("  ")
+        temp -= 1
+
+
 for i in range(0, lineCounter - 1):
-    if lines[i].lstrip()[0] == '-':
-        f_out.write('    "' + lines[i].lstrip().lstrip('-'))
+    if i == 0:
+        lastLevel = 0
+        thisLevel = len(lines[i]) - len(lines[i].lstrip())
     else:
-        keyword = lines[i].lstrip().split(':', maxsplit=1)
-        f_out.write('    "' + keyword[0] + '": "' + keyword[1].lstrip())
-    nextLevel = len(lines[i + 1]) - len(lines[i + 1].lstrip())
-    if nextLevel < lastLevel:
-        f_out.write("  },"'\n')
-    if nextLevel > lastLevel:
-        f_out.write('"\n'"  {"'\n')
-    lastLevel = nextLevel
-f_out.write('  }\n}\n')
+        lastLevel = len(lines[i - 1]) - len(lines[i - 1].lstrip())
+        thisLevel = len(lines[i]) - len(lines[i].lstrip())
+    if lastLevel < thisLevel:
+        level += (thisLevel-lastLevel)//2
+    if lastLevel > thisLevel:
+        level -= (lastLevel-thisLevel)//2
+        if bigBrackets[level] != 0:
+            write_space(level)
+            if lines[i+1] == '' or lines[i+1] == '\n':
+                f_out.write('}\n')
+                bigBrackets[level] -= 1
+            else:
+                f_out.write('},\n')
+                bigBrackets[level] -= 1
+        if middleBrackets[level] != 0:
+            write_space(level)
+            f_out.write(']\n')
+            middleBrackets[level] -= 1
+
+    print("level=", level, "lastLevel=", lastLevel, "thisLevel=", thisLevel)
+    if ':' in lines[i]:
+        keyword = lines[i].strip().split(':', maxsplit=1)
+        print("|keyword0="+keyword[0]+"|keyword1="+keyword[1])
+        if keyword[1].strip() == "":
+            write_space(level)
+            f_out.write('"' + keyword[0].strip() + '": [\n')
+            print("'['at level", level)
+            middleBrackets[level] += 1
+        elif lines[i+1].strip() == '-':
+            write_space(level)
+            f_out.write('"' + keyword[0].strip() + '": "' + keyword[1].strip() + '"\n')
+        elif (lines[i+1] == '\n') or (lines[i+1] == ''):
+            write_space(level)
+            f_out.write('"' + keyword[0].strip() + '": "' + keyword[1].strip() + '"\n')
+            write_space(level-1)
+            f_out.write('}\n')
+        else:
+            write_space(level)
+            f_out.write('"' + keyword[0].strip() + '": "' + keyword[1].strip() + '",\n')
+    if lines[i].strip() == '-':
+        write_space(level)
+        f_out.write('{\n')
+        print("'{'at level", level)
+        bigBrackets[level] += 1
+f_out.write('}\n')
+
+f_in.close()
 f_out.close()
