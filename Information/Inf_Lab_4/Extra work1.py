9f77506450b428975636249848b4cd5af0f23199
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-12-10 20:03:51 +0300

    update

--- Information/Inf_Lab_4/Extra work1.py
+++ Information/Inf_Lab_4/Extra work1.py
@@ -1,1 +1,10 @@
 import yaml
+import json
+
+f_in = open('source file.yml', 'r', encoding='utf-8')
+f_out = open('output file.json', 'w')
+
+content = yaml.load(stream=f_in, Loader=yaml.SafeLoader)
+print(content)
+
+f_out.write(json.dumps(content))
