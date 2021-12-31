6fe24177db63dad2be7462cfad6798d09b57c1a3
托利亚 <45510494+Tolia-GH@users.noreply.github.com>
2021-12-31 10:59:18 +0300

    Update Thing.java

--- Program/Prog_Lab_4/src/Object/Thing.java
+++ Program/Prog_Lab_4/src/Object/Thing.java
@@ -1,9 +1,29 @@
 package Object;
 
 public class Thing {
+
+    Character[] participants = new Character[7];
+    private int numParticipants = 0;
+
     public enum State {
         GOOD,
         BAD,
         UNKNOWN
+    }
+
+    public void takePartIn(Character character) {
+
+    }
+
+    public void getWorse() {
+
+    }
+
+    public Character[] getParticipants() {
+        return this.participants;
+    }
+
+    public int getNumParticipants() {
+        return numParticipants;
     }
 }
